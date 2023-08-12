550bfc0a0363b113a975ce147f1efe3996bb44eb
muhammetozdemir10 <127602475+muhammetozdemir10@users.noreply.github.com>
2023-08-12 19:44:15 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace MoneyCounter;
+
+class Program
+{
+    static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("Para sayma uygulamasina hosgeldiniz");
+        Console.ResetColor();
+
+        int[] moneyUnits = { 200, 100, 50, 20, 10, 5, 1 };
+
+        try
+        {
+            Console.WriteLine("Lutfen para miktarini giriniz");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            foreach (int moneyUnit in moneyUnits)
+            {
+                int dividedNumber = input / moneyUnit;
+                input -= dividedNumber * moneyUnit;
+                Console.WriteLine($"{moneyUnit} = {dividedNumber}");
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Yanlis girdiniz lutfen kontrol ediniz");
+        }
+    }
+}
